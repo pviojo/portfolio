644199775876ff59b107b88a7e260affda939d63
Pablo Viojo <pviojo@gmail.com>
2023-08-04 12:11:22 -0400

    Add Matomo tracking

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -40,6 +40,20 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 
           gtag('config', 'G-T9M1XZ8MZP');`}
             </Script>
+
+            <Script id='matomo-analytics'>
+              {`var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://matomo.pablo-viojo.com/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();`}
+            </Script>
           </>
         ) : null}
         <Providers>
