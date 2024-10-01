107492c7eea225812d3a4c064545768d61d071fd
Pablo Viojo <pviojo@gmail.com>
2024-09-30 22:33:43 -0300

    Revalidate cache on save

--- src/app/(admin)/admin/posts/actions.ts
+++ src/app/(admin)/admin/posts/actions.ts
@@ -1,8 +1,8 @@
-"use server";
+'use server';
 
-import {createPost, updatePost} from "@/lib/database/posts";
-import {revalidatePath} from "next/cache";
-import {Post} from "@prisma/client";
+import {createPost, updatePost} from '@/lib/database/posts';
+import {revalidatePath} from 'next/cache';
+import {Post} from '@prisma/client';
 
 export async function saveOrUpdatePost(post: Post) {
   if (post.id) {
@@ -13,7 +13,8 @@ export async function saveOrUpdatePost(post: Post) {
     }
     await createPost(post);
   }
-  revalidatePath("/posts");
-  revalidatePath("/");
+  revalidatePath('/posts');
+  revalidatePath('/admin');
+  revalidatePath('/');
   return true;
 }
