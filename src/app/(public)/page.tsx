7816656bf384f1ce7c8ba6caf38a3dd2480d04eb
Pablo Viojo <pviojo@gmail.com>
2024-10-21 18:45:06 -0300

    Change to cofounder at Teambit

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -99,7 +99,7 @@ export default async function Home() {
               </div>
               <div className='border-gray-200 dark:border-gray-600 border-b mb-8'>
                 {[
-                  [2023, null, 'First Engineer at Teambit'],
+                  [2023, null, 'Cofounder at Teambit'],
                   [2021, 2023, 'Tech Leader at Grin Scooters'],
                   [2017, 2021, 'Former COO and CTO at Groupon Latam'],
                   [2015, 2017, 'Cofounder at Latam Builders'],
